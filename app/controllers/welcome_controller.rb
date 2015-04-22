bab6b4d17c834904bcb7e73a3f6ebf38329e68c4
cuffery <cuffery@hotmail.com>
2015-04-21 20:52:01 -0400

    idk

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def home
+    if logged_in?
+      @myAcceptedTasks = Task.where(runner_id: current_user.id)
+      @myPublishedTasks = Task.where(user_id: current_user.id)
+    else
+      @myAcceptedTasks = 0
+      @myPublishedTasks = 0
+    end
   end
 
   def about
